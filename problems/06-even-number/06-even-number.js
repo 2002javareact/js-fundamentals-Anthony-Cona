6a224f430855f3ff2b7a3d9fce262374b856d558
Anthony-Cona <acon5000@gmail.com>
2020-02-13 15:43:53 -0500

    Added if statement to return value if number is even

--- problems/06-even-number/06-even-number.js
+++ problems/06-even-number/06-even-number.js
@@ -3,5 +3,14 @@ Define function: isEven(someNum)
 Return true if even, false if odd.
 Do not use % operator. */
 function isEven(someNum) {
-  
+    if (Number.isInteger(someNum / 2)) {
+        return true
+    } else {
+        return false
+    }
 }
+
+console.log(isEven(3));
+console.log(isEven(4));
+console.log(isEven(9));
+console.log(isEven(98432));

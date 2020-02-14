86ac99f0541717b20dbefca0e2e782ec4e39bb0a
Anthony-Cona <acon5000@gmail.com>
2020-02-14 09:44:26 -0500

    Inital writen code for project

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -2,5 +2,18 @@
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
 function isPalindrome(someStr) {
-  
+    for (let index = 0; index < someStr.length/2; index++) {
+
+        if (someStr.charAt(index) !== someStr.charAt(someStr.length - index - 1)) {
+            return false
+        }
+
+        return true        
+    }
 }
+
+console.log(isPalindrome("racecar"));
+console.log(isPalindrome("taco cat"));
+console.log(isPalindrome("racecars"));
+
+

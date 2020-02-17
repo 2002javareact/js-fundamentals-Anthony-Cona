74e8592a9f7c6e1af92e45d6d258adff3bb517ae
Anthony-Cona <acon5000@gmail.com>
2020-02-16 21:27:34 -0500

    iterative bubble sort with test cases

--- problems/02-bubble-sort/02-bubble-sort.js
+++ problems/02-bubble-sort/02-bubble-sort.js
@@ -3,5 +3,24 @@ Define function: bubbleSort(numArray)
 Use the bubble sort algorithm to sort the array.
 Return the sorted array. */
 function bubbleSort(numArray) {
-  
+    //This loop checks the array for each index in the array,
+    //it allows for lower values to move as far left as possible
+    for (let i = 0; i < numArray.length; i++) {
+        //This loop checks the value at each index and swaps it if 
+        //the value on the left is larger then the value on the right
+        for (let j = 0; j < numArray.length; j++) {
+            if (numArray[j] > numArray[j + 1]) {
+                let temp = numArray[j]
+                numArray[j] = numArray[j + 1]
+                numArray[j + 1] = temp
+            }
+        }
+    }
+    return numArray
 }
+
+//worst case
+console.log(bubbleSort([6,5,4,3,2,1]));
+//best case
+console.log(bubbleSort([1,2,3,4,5,6]));
+console.log(bubbleSort([77,22,398,344,76,1212]));

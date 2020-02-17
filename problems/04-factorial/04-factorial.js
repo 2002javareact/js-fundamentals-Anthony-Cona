a8e6386666e1945462667c2c6961f5d6e5e0facb
Anthony-Cona <acon5000@gmail.com>
2020-02-16 21:47:09 -0500

    Recursive factorial with test code

--- problems/04-factorial/04-factorial.js
+++ problems/04-factorial/04-factorial.js
@@ -2,5 +2,15 @@
 Define function: factorial(someNum)
 Use recursion to compute and return the factorial of someNum.  */
 function factorial(sumNum) {
-  
+    //Base case, needs to be 1 if it was 0 or lower would not return the correct value
+    if (sumNum === 1) {
+        return (sumNum)
+    }
+    //recursively takes sumNum-1 and multiplies it by the value+1
+    return (sumNum * factorial(sumNum - 1))
 }
+
+console.log(factorial(3));
+console.log(factorial(4));
+console.log(factorial(5));
+console.log(factorial(6));

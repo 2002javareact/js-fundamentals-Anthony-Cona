579cd78df0b48012f2e5270f7997187783229363
Anthony-Cona <acon5000@gmail.com>
2020-02-16 22:07:54 -0500

    Prints Objects into terminal, used object from training

--- problems/09-object-properties/09-object-properties.js
+++ problems/09-object-properties/09-object-properties.js
@@ -2,5 +2,20 @@
 Define function objectProperties(someObj)
 Print every property and it's value. */
 function objectProperties(someObj) {
-
+    //By returning the log of the object JS prints all the elements and their values
+    return console.log(someObj);        
 }
+
+//Used Object from traing for this problem
+const circle = {
+    radius: 10,
+    area: 314.15,
+    color:{
+        id:1 ,
+        color: 'red'},
+        spin: ()=>{
+            console.log('wee');
+        }
+    }
+
+    objectProperties(circle)

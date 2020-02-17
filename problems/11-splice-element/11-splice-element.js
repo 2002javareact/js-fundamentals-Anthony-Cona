32373b4d8d2294e1e17b7c8e4c18cc3038553d7c
Anthony-Cona <acon5000@gmail.com>
2020-02-16 23:06:39 -0500

    Splices element from array and returns result

--- problems/11-splice-element/11-splice-element.js
+++ problems/11-splice-element/11-splice-element.js
@@ -3,5 +3,15 @@ Define function spliceElement(someArr, index)
 Splice the element at the provided index in the array.
 */
 function spliceElement(someArr, index) {
-  
+    //Splice removes an element at a given array
+    someArr.splice(index,1)
+    //returning the copy of the array now exclueds the spliced element
+    return someArr
 }
+
+//Test Code
+let numArray = [2,3,4,5,6]
+let strArray = ["Potato","Carrot","Leek","Apple","Banana"]
+console.log(spliceElement(numArray,2));
+console.log(spliceElement(strArray,1))
+
